25cc3fcf4dcbc37753bf8fcca467defdff8e31ab
BernadDwiki <125329942+BernadDwiki@users.noreply.github.com>
2023-02-17 17:47:41 +0700

    menambahkan interpolation search

--- searchAndSort.java
+++ searchAndSort.java
@@ -31,4 +31,32 @@ public class searchAndSort {
         }
         return -1;
     }
+    
+    public static int interpolationSearch(int arr[], int lo,
+            int hi, int x) {
+
+        int pos;
+
+        if (lo <= hi && x >= arr[lo] && x <= arr[hi]) {
+
+            pos = lo
+                    + (((hi - lo) / (arr[hi] - arr[lo]))
+                    * (x - arr[lo]));
+
+            if (arr[pos] == x) {
+                return pos;
+            }
+
+            if (arr[pos] < x) {
+                return interpolationSearch(arr, pos + 1, hi,
+                        x);
+            }
+
+            if (arr[pos] > x) {
+                return interpolationSearch(arr, lo, pos - 1,
+                        x);
+            }
+        }
+        return -1;
+    }
 }
